5a21130b6b8cdde3d1bd4dc21535b2989723b931
Mert Tecimen <merttecimen@yahoo.com.tr>
2022-05-15 21:02:01 +0300

    Added MainView, door textures and Door Object

--- Week 1 Homework.playground/Contents.swift
+++ Week 1 Homework.playground/Contents.swift
@@ -1,11 +1,36 @@
-import UIKit
+import SwiftUI
+import PlaygroundSupport
 import Foundation
 
+enum DoorState{
+    case opened, closed
+}
 
-
-class TextProcessor{
+struct DoorObject: View{
     
+    @State var currentState: DoorState = .closed
     
+    var body: some View{
+        Image(uiImage: UIImage(named: currentState == .closed ? "Closed Door.png" : "Opened Door.png")!)
+    }
     
 }
+
+struct MainView: View{
+    var body: some View{
+        
+        DoorObject()
+        
+    }
+}
+
+
+
+
+
+PlaygroundPage.current.setLiveView(
+    MainView()
+        .frame(width: 400, height: 400)
+)
+
 
