fa131b98f96dfab91650006e45d68d247dd0c73e
Mert Tecimen <merttecimen@yahoo.com.tr>
2022-05-16 00:12:02 +0300

    Added game reset method and converted if statements to switch for game state.

--- Week 1 Homework.playground/Contents.swift
+++ Week 1 Homework.playground/Contents.swift
@@ -5,7 +5,7 @@ import Foundation
 class Game: ObservableObject{
     
     
-    var numberOfDoor: Int
+    @Published var numberOfDoor: Int
     @Published var indexWithPrize: Int
     @Published var indexForDecision: Int?
     @Published var gameState: GameState
@@ -15,7 +15,6 @@ class Game: ObservableObject{
         self.numberOfDoor = numberOfDoor
         self.gameState = .begining
         self.indexWithPrize = Int.random(in: 0..<numberOfDoor)
-        print("Prize is in Door \(self.indexWithPrize + 1)")
     }
     
     func selectIndexForDecision(){
@@ -27,8 +26,16 @@ class Game: ObservableObject{
             self.indexForDecision = indexForDecision
             return
         }
-        self.indexForDecision = self.selectedIndex
+        self.indexForDecision = self.indexWithPrize
         return
+    }
+    
+    func resetGame(numberOfDoors: Int){
+        self.numberOfDoor = numberOfDoors
+        self.indexWithPrize = Int.random(in: 0..<numberOfDoors)
+        self.indexForDecision = nil
+        self.selectedIndex = nil
+        self.gameState = .begining
     }
     
 }
@@ -56,10 +63,21 @@ struct DoorObject: View{
         ZStack{
         
             Image(uiImage: UIImage(named: currentState == .closed ? "Closed Door.png" : "Opened Door.png")!)
-                .onChange(of: self.selection){_ in
-                    if self.indexForDecision! != self.doorIndex && !self.isContainPrize{
+                .onChange(of: self.gameState){_ in
+                    
+                    
+                    switch (self.gameState){
+                    case .begining:
+                        self.currentState = .closed
+                    case .decisioning:
+                        if self.indexForDecision! != self.doorIndex && !self.isContainPrize && self.selection != self.doorIndex{
+                            self.currentState = .opened
+                        }
+                    case .ending:
                         self.currentState = .opened
+                        
                     }
+
                 }
             if self.currentState == .opened{
                 if self.isContainPrize{
@@ -84,6 +102,7 @@ struct DoorObject: View{
 struct MainView: View{
     
     @ObservedObject var game: Game = Game(numberOfDoor: 4)
+    @State var endMessage: String?
     
     var body: some View{
         
@@ -95,7 +114,7 @@ struct MainView: View{
             Spacer()
             HStack{
                 
-                ForEach(0..<game.numberOfDoor){ index in
+                ForEach(0..<game.numberOfDoor, id: \.self){ index in
                     VStack{
                         Text("Door \(index + 1)")
                         DoorObject(selection: $game.selectedIndex, indexForDecision: $game.indexForDecision, gameState: $game.gameState, doorIndex: index, isContainPrize: game.indexWithPrize == index)
@@ -103,11 +122,50 @@ struct MainView: View{
                 }
             }
             Spacer()
+            
+            switch (game.gameState){
+            case .decisioning:
+                if game.indexForDecision != nil{
+                    VStack(spacing: 20){
+                        Text("Would you like to go on with Door \(game.selectedIndex! + 1) or switch to Door \(game.indexForDecision! + 1)?")
+                        
+                        HStack(spacing: 50){
+                            Button("Go On"){
+                                if game.selectedIndex == game.indexWithPrize{
+                                    self.endMessage = "You win the prize!"
+                                } else {
+                                    self.endMessage = "You are now a proud owner of a duck!"
+                                }
+                                game.gameState = .ending
+                            }
+                            
+                            Button("Switch"){
+                                if game.indexForDecision == game.indexWithPrize{
+                                    self.endMessage = "You win the prize!"
+                                } else {
+                                    self.endMessage = "You are now a proud owner of a duck!"
+                                }
+                                game.gameState = .ending
+                            }
+                        }
+                    }
+                }
+            case .ending:
+                VStack(spacing: 20){
+                    Text(self.endMessage!)
+                    Button("Play Again"){
+                        self.game.resetGame(numberOfDoors: 4)
+                    }
+                }
+            case .begining:
+                Text("Select a door!")
+                Text("")
+            }
+            Spacer()
         }
         .onChange(of: game.gameState){ _ in
             if game.gameState == .decisioning{
                 game.selectIndexForDecision()
-                print("Index for decision: \(game.indexForDecision! + 1)")
             }
         }
     }
